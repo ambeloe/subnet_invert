2bd43de629fa16c251e9fb93598448dc0d89eed2
Nick B. <ambeloe@gmail.com>
2025-03-22 04:51:59 -0400

    multiple

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 extern crate core;
 
+use cidr::{Cidr, Ipv4Cidr};
 use std::env;
 use std::net::Ipv4Addr;
 use std::process::ExitCode;
 use std::str::FromStr;
-use cidr::{Cidr, Ipv4Cidr};
 
 fn main() -> ExitCode {
     let mut subnets: Vec<Ipv4Cidr> = Vec::new();
@@ -14,39 +14,37 @@ fn main() -> ExitCode {
             subnets.push(ip.unwrap());
         } else {
             println!("invalid subnet: {}", a);
-            println!("provide one or more subnets separated by a space, and the minimum \"inverse\" will be output (subnet_invert 64.0.0.0/3 ==> 0.0.0.0/2 96.0.0.0/3 128.0.0.0/1)");
-            return  ExitCode::FAILURE
+            println!(
+                "provide one or more subnets separated by a space, and the minimum \"inverse\" will be output (subnet_invert 64.0.0.0/3 ==> 0.0.0.0/2 96.0.0.0/3 128.0.0.0/1)"
+            );
+            return ExitCode::FAILURE;
         }
     }
-    
+
     let mut supernet: Vec<Ipv4Cidr> = Vec::new();
-    for s in &subnets {
-        let mut inv: Vec<Ipv4Cidr> = Vec::new();
-        
-        invert_subnet(
-            *s,
-            Ipv4Cidr::new(
-                Ipv4Addr::from_bits(0), 
-                0
-            ).unwrap(),
-            &mut inv
-        );
-        
-        supernet = merge_subnets(supernet, inv);
-    }
-    
+    invert_subnet(
+        &subnets,
+        Ipv4Cidr::new(Ipv4Addr::from_bits(0), 0).unwrap(),
+        &mut supernet,
+    );
+
     supernet.sort_by_key(|c| c.network_length());
 
-    // let mut blocked: u128 = 0;
-    // let mut allowed: u128 = 0;
-    // for s in &subnets{
-    //     allowed += 2u128.pow((32 - s.network_length()) as u32);
-    // }
-    // for s in &supernet {
-    //     blocked += 2u128.pow((32 - s.network_length()) as u32);
-    // }
-    // println!("input {}, output : {}, total: {}", allowed, blocked, allowed+blocked);
-    
+    let mut blocked: u128 = 0;
+    let mut allowed: u128 = 0;
+    for s in &subnets {
+        allowed += 2u128.pow((32 - s.network_length()) as u32);
+    }
+    for s in &supernet {
+        blocked += 2u128.pow((32 - s.network_length()) as u32);
+    }
+    println!(
+        "input {}, output : {}, total: {}",
+        allowed,
+        blocked,
+        allowed + blocked
+    );
+
     for s in supernet {
         print!("{} ", s);
     }
@@ -56,43 +54,43 @@ fn main() -> ExitCode {
 
 /// target: subnet you want to find the inverse of
 /// pos: where to start the search from (probably want this to be 0.0.0.0/0 if calling it yourself)
-fn invert_subnet(target :Ipv4Cidr, pos:Ipv4Cidr, output: &mut Vec<Ipv4Cidr>){
+fn invert_subnet(target: &Vec<Ipv4Cidr>, pos: Ipv4Cidr, output: &mut Vec<Ipv4Cidr>) {
     //smallest subnet possible
-    if target == pos || pos.first_address() == pos.last_address(){
-        return
+    if pos.first_address() == pos.last_address() {
+        return;
     }
-    
+
     //split bigger subnet into two halves
-    let front = Ipv4Cidr::new(pos.first_address(), pos.network_length()+1).unwrap();
+    let front = Ipv4Cidr::new(pos.first_address(), pos.network_length() + 1).unwrap();
     let back = Ipv4Cidr::new(
         Ipv4Addr::from_bits(
-            pos.first_address().to_bits()+
-                (((pos.last_address().to_bits()-pos.first_address().to_bits())/2)+1),
+            pos.first_address().to_bits()
+                + (((pos.last_address().to_bits() - pos.first_address().to_bits()) / 2) + 1),
         ),
-        pos.network_length()+1
-    ).unwrap();
-    
-    let fc = front.contains(&target.first_address()) && front.contains(&target.last_address());
-    let bc = back.contains(&target.first_address()) && back.contains(&target.last_address());
-    
-    if fc && !bc { //in front
+        pos.network_length() + 1,
+    )
+    .unwrap();
+
+    let mut fc: bool = false;
+    let mut bc: bool = false;
+    for i in target {
+        if *i == pos {
+            continue;
+        }
+        fc |= front.contains(&i.first_address()) && front.contains(&i.last_address());
+        bc |= back.contains(&i.first_address()) && back.contains(&i.last_address());
+    }
+
+    if fc && !bc {
         output.push(back);
         invert_subnet(target, front, output);
-    } else if bc && !fc { //in back
+    } else if bc && !fc {
         output.push(front);
         invert_subnet(target, back, output);
     } else if fc && bc {
-        return
+        invert_subnet(target, front, output);
+        invert_subnet(target, back, output);
     } else {
-        output.push(pos)
+        return
     }
-}
-
-fn merge_subnets(a: Vec<Ipv4Cidr>, b: Vec<Ipv4Cidr>) -> Vec<Ipv4Cidr> {
-    let mut e: Vec<Ipv4Cidr> = Vec::new();
-    
-    e.extend(a);
-    e.extend(b);
-    
-    e
 }
